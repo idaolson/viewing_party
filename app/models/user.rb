fd74c02faeecddf0655bfac78b1439eabc12c9d3
Ida Olson <idaolson@gmail.com>
2021-10-12 17:27:36 -0600

    Update relationships in user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :friendships, dependent: :destroy
   has_many :invitations, dependent: :destroy
-  has_many :events, through: :invitations
+  has_many :events
   has_many :friends, through: :friendships
+  has_many :invited_events, through: :invitations, source: :event
 
   has_secure_password
 
