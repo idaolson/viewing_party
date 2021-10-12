1e4590d7a504accfd1af2c000f4b8a622a8d0925
Ida Olson <idaolson@gmail.com>
2021-10-12 17:27:24 -0600

    Add additional relationship to user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User, type: :model do
     it { should have_many(:friendships) }
     it { should have_many(:invitations) }
     it { should have_many(:friends).through(:friendships) }
+    it { should have_many(:invited_events).through(:invitations) }
   end
 
   describe 'instance methods' do
