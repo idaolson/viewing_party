678b57f0a36fe4dc1691dbab0b76e239dd9e1a62
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-12 15:26:22 -0400

    Add has_secure_password to user model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
-
   has_many :invitations, dependent: :destroy
   has_many :users, through: :invitations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :invitations, dependent: :destroy
   has_many :events, through: :invitations
   has_many :friends, through: :friendships
+
+  has_secure_password
 end
