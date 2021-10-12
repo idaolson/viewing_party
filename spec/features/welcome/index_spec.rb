0c315f964b127153fd6496877c3552ce6842e275
Ida Olson <idaolson@gmail.com>
2021-10-12 14:40:05 -0600

    Add welcome index tests

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,6 +4,28 @@ RSpec.describe "Welcome Index" do
   it "welcomes" do
     visit root_path
 
-    expect(page).to have_content("HELLO AWESOME VIEWERS")
+    expect(page).to have_content("Welcome to Awesome Viewing Party!")
+  end
+
+  it "creates a new user" do
+    visit root_path
+
+    click_on "Register Here"
+
+    expect(current_path).to eq(new_user_path)
+
+    test_email = "idaolson@gmail.com"
+    test_pass = "Bianca"
+    test_name = "Ida"
+
+    save_and_open_page
+    fill_in 'user[name]', with: test_name
+    fill_in 'user[email]', with: test_email
+    fill_in 'user[password]', with: test_pass
+
+    click_on "Register"
+
+    expect(page).to have_content("Welcome to Viewing Party, #{test_name}!")
+    expect(current_path).to eq(dashboard_index_path)
   end
 end
