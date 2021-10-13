63d64e22bbc62284e533ebec5fa2c678d1db50e7
Ida Olson <idaolson@gmail.com>
2021-10-13 17:40:20 -0600

    Add test for discover index

---
+++ spec/features/discover/index_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "discover movies page" do
+  it "has a button for top rated movies" do
+    expect(page).to have_button("Find Top Rated Movies")
+
+    click_button("Find Top Rated Movies")
+
+    expect(current_path).to eq(movies_path)
+  end
+end
