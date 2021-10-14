d4a88ae914205f48ba933c5226c0b8cf7cca8bfe
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-14 13:52:02 -0400

    Add a conditional in movie controller for search params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,12 @@
 class MoviesController < ApplicationController
   def index
     movie_facade = MovieFacade.new
-    @movies = movie_facade.top_rated_movies
     @user = current_user
+
+    if params[:search].present?
+      @movies = movie_facade.search_by_title(params[:search])
+    else
+      @movies = movie_facade.top_rated_movies
+    end
   end
 end
