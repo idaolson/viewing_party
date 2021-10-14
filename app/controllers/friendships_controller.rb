1dd8883e8bafbf92bd4057072ff55d9a27590f53
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-14 10:08:27 -0400

    Remove @ from certain variables

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,9 @@
 class FriendshipsController < ApplicationController
   def create
     @user = current_user
-    @friend = User.find_by(email: params[:email])
-    if @friend
-      @friendship = Friendship.create!(user_id: @user.id, friend_id: @friend.id)
+    friend = User.find_by(email: params[:email])
+    if friend
+      friendship = Friendship.create!(user_id: @user.id, friend_id: friend.id)
       redirect_to dashboard_index_path
     else
       flash[:error] = "Friend not found."
