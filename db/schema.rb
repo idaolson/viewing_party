b473df8f2d1bb3299e63bdfb9145efc762f9d6e3
Ida Olson <idaolson@gmail.com>
2021-10-16 15:05:32 -0600

    Add movie id column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_181849) do
+ActiveRecord::Schema.define(version: 2021_10_16_172718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_10_12_181849) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "movie_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
