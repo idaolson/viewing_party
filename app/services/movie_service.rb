4e78f1d5ffe1acbb661eb871bb85e870460a2938
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-18 15:51:09 -0400

    Change endpoint for top_rated movies

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,7 +1,7 @@
 class MovieService
   def top_movies
-    page1 = MovieClient.get_data("/3/discover/movie?language=en-US&sort_by=vote_average.desc&page=1")
-    page2 = MovieClient.get_data("/3/discover/movie?language=en-US&sort_by=vote_average.desc&page=2")
+    page1 = MovieClient.get_data("/3/movie/top_rated?language=en-US&page=1")
+    page2 = MovieClient.get_data("/3/movie/top_rated?language=en-US&page=2")
     page1[:results] + page2[:results]
   end
 
