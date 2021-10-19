caa1e295561a83abb88b26aac5609b6581ac022d
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-19 13:40:31 -0400

    Add a conditional for movie search for upcoming movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,8 @@ class MoviesController < ApplicationController
 
     if params[:search].present?
       @movies = movie_facade.search_by_title(params[:search])
+    elsif params[:upcoming].present?
+      @movies = movie_facade.upcoming_movies
     else
       @movies = movie_facade.top_rated_movies
     end
@@ -16,5 +18,6 @@ class MoviesController < ApplicationController
     @user = current_user
     @cast = movie_facade.cast_by_id(params[:id])
     @reviews = movie_facade.review_by_id(params[:id])
+    @recommended = movie_facade.recommended_by_id(params[:id])
   end
 end
