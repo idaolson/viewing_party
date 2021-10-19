6b6df46f04996c37b53819c89a40988a06df2f2a
Ida Olson <idaolson@gmail.com>
2021-10-18 18:07:45 -0600

    Add validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,10 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :invitations, dependent: :destroy
   has_many :users, through: :invitations
+
+  validates :movie_id, presence: true
+  validates :title, presence: true
+  validates :day, presence: true
+  validates :time, presence: true
+  validates :duration, presence: true
 end
